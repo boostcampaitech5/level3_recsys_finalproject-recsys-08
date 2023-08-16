a44a7b707827093f3dc1938f18f2f17b5f52c767
Kim JiWoo <rlawldn11703@gmail.com>
2023-08-16 17:45:00 +0900

    [Refactor] Fix parameter & Remove unnecessary code
     #29
    
    Signed-off-by: Kim JiWoo <rlawldn11703@gmail.com>

--- top_k_recommendation/model/inference.py
+++ top_k_recommendation/model/inference.py
@@ -7,14 +7,15 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
 
-from annoy import AnnoyIndex
+from torch.utils.data import DataLoader
+from .dataset import *
+from .annoy import *
 
 class Inference:
-    def __init__(self,args, key, transformer, annoy_index, count_dict):
+    def __init__(self,args, corp_seq,key, transformer, annoy_index, count_dict,key2idx):
         self.args = args
+        self.corp_seq = corp_seq
         self.n_trees = self.args.n_trees
         self.n = self.args.n
         self.d_embed = self.args.d_embed
@@ -22,11 +23,12 @@ class Inference:
         self.model = transformer
         self.annoy_index = annoy_index
         self.device = self.args.device
+        self.key2idx = key2idx
         
     def inference(self):
         device = torch.device(self.device)
-        idx = corp2idx[self.key]
-        inference_dataset = Dataset(args, corp_seq, data_type="inference")[idx]
+        idx = self.key2idx[self.key]
+        inference_dataset = Dataset(self.args, self.corp_seq, data_type="inference")[idx]
         inference_dataloader = DataLoader(inference_dataset, batch_size= 1)
         with torch.no_grad():
             self.model.eval()
@@ -38,9 +40,9 @@ class Inference:
         output = output.reshape(-1,1)
         return output
     
-    def scoring(self):
+    def scoring(self,df_biz):
         self.inference_vector = self.inference()
-        annoy = Annoy(args, self.annoy_index)
+        annoy = Annoy(self.args, self.annoy_index)
         ann_idx,ann_score = annoy.find_annoy(self.inference_vector) # (768, 1)
         score_dict = {}
         for idx,score in zip(ann_idx,ann_score):
@@ -52,24 +54,20 @@ class Inference:
                 
         return score_dict
     
-    def find_candidates(self):
-        score_dict = self.scoring()
+    def find_candidates(self,df_biz):
+        score_dict = self.scoring(df_biz)
         k = len(score_dict)
-        # corp_list = np.repeat(df_biz[df_biz['key']==self.key]['회사명'].iloc[0],k)
-        corp_list = np.repeat(key2corpname[self.key],k)
         key_list = np.repeat(self.key, k)
         sorted_score = sorted(score_dict.items(), key=lambda x: (x[1][0],x[1][1]), reverse=True)
-        target_corp_list, target_key_list = [],[]
+        target_key_list = []
         for target_corp_info in sorted_score:
             target_corp_key = target_corp_info[0]
-            target_corp = key2corpname[target_corp_key]
-            target_corp_list.append(target_corp)
             target_key_list.append(target_corp_key)
-        df_candidate = pd.DataFrame({'의뢰기업':corp_list,'의뢰기업_key':key_list,'대상기업':target_corp_list,'대상기업_key':target_key_list})
+        df_candidate = pd.DataFrame({'의뢰기업_key':key_list,'대상기업_key':target_key_list})
         return df_candidate
     
     def filtering(self,inference):
-        df_value = pd.read_csv('./corp_valuation_v2.csv')
+        df_value = pd.read_csv('../../corp_valuation_v2.csv')
         value_dict = dict(zip(df_value['회사명'],df_value['기업가치']))
         inference['의뢰기업_기업가치'] = inference['의뢰기업_key'].map(value_dict)
         inference['추천회사_기업가치'] = inference['대상기업_key'].map(value_dict)
